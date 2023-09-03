00abf520d07ffd1c588513a9e1f4b0d1def09514
TristanSergey <tristan.sergey@gmail.com>
2023-09-03 13:54:12 +0300

    03092023

--- task_200/task_120/task_118_m_62.cpp
+++ task_200/task_120/task_118_m_62.cpp
@@ -21,7 +21,9 @@ int uniquePaths(int m, int n)
     for (int i = 1; i < m; i++)
     {
         for (int j = 1; j < n; j++)
+
         {
+
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
