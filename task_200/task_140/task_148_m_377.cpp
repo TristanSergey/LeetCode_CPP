b1a51ef27c94362c3d0d1539e175efed0b3900aa
TristanSergey <tristan.sergey@gmail.com>
2023-09-09 18:00:59 +0300

    09092023

---
+++ task_200/task_140/task_148_m_377.cpp
@@ -0,0 +1,42 @@
+﻿// Учитывая массив различных целых чисел nums и целевое целое число target,
+// вернуть количество возможных комбинаций, которые в сумме дают target.
+// Тестовые примеры генерируются таким образом, чтобы ответ мог уместиться в 32 - битном целом числе.
+// 377
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <unordered_map>
+#include <string>
+#include <utility>
+using namespace std;
+
+int combinationSum4(vector<int> &nums, int target)
+{
+    vector<unsigned int> dp(target + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++)
+    {
+        for (int num : nums)
+        {
+            if (i - num >= 0)
+            {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+    return dp[target];
+};
+
+int main()
+{
+    int target = 5;
+    vector<int> nums = {1, 2, 3};
+    bool result = combinationSum4(nums, target);
+
+    std::cout << result << " ";
+
+    std::cout << std::endl;
+    return 0;
+}
---
+++ task_200/task_150/task_150_m_416.cpp
@@ -0,0 +1,56 @@
+﻿// Учитывая целочисленный массив nums, верните true, если вы можете разделить массив на два подмножества так,
+// чтобы сумма элементов в обоих подмножествах была равна или false в противном случае.
+// 416
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <unordered_map>
+#include <string>
+#include <utility>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+bool canPartition(vector<int> &nums)
+{
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % 2 != 0)
+    {
+        return false;
+    }
+    int target = sum / 2;
+    int n = nums.size();
+    vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+    dp[0][0] = true;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= target; ++j)
+        {
+            if (j >= nums[i - 1])
+            {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[n][target];
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> nums = {4, 4, 4, 4, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8, 8, 8, 12, 12, 12, 12, 12, 12, 12, 12, 16, 16, 16, 16, 16, 16, 16, 16, 20, 20, 20, 20, 20, 20, 20, 20, 24, 24, 24, 24, 24, 24, 24, 24, 28, 28, 28, 28, 28, 28, 28, 28, 32, 32, 32, 32, 32, 32, 32, 32, 36, 36, 36, 36, 36, 36, 36, 36, 40, 40, 40, 40, 40, 40, 40, 40, 44, 44, 44, 44, 44, 44, 44, 44, 48, 48, 48, 48, 48, 48, 48, 48, 52, 52, 52, 52, 52, 52, 52, 52, 56, 56, 56, 56, 56, 56, 56, 56, 60, 60, 60, 60, 60, 60, 60, 60, 64, 64, 64, 64, 64, 64, 64, 64, 68, 68, 68, 68, 68, 68, 68, 68, 72, 72, 72, 72, 72, 72, 72, 72, 76, 76, 76, 76, 76, 76, 76, 76, 80, 80, 80, 80, 80, 80, 80, 80, 84, 84, 84, 84, 84, 84, 84, 84, 88, 88, 88, 88, 88, 88, 88, 88, 92, 92, 92, 92, 92, 92, 92, 92, 96, 96, 96, 96, 96, 96, 96, 96, 97, 99};
+    bool result = canPartition(nums);
+
+    std::cout << result << " ";
+
+    std::cout << std::endl;
+    return 0;
+}
